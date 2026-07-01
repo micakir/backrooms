1c9f13e529632cab8663bc9753f9e2df55f6d6e1
micakir <micaakir@gmail.com>
2026-07-01 14:21:44 -0300

    Initial commit

---
+++ Assets/Scripts/Flashlight.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class Flashlight : MonoBehaviour
+{
+    public Light flashLight;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            flashLight.enabled = !flashLight.enabled;
+        }
+    }
+}
