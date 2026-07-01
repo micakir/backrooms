907edc778e6d1a0e1c0bd3dcf2d1183d7ac8dbb9
micakir <micaakir@gmail.com>
2026-07-01 14:30:09 -0300

    chanchanchaaaan

--- Assets/Scripts/Flashlight.cs
+++ Assets/Scripts/Flashlight.cs
@@ -3,12 +3,24 @@
 public class Flashlight : MonoBehaviour
 {
     public Light flashLight;
+    public float battery = 100f;
+    public float drainSpeed = 4f;
 
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.F))
         {
             flashLight.enabled = !flashLight.enabled;
+        }
+
+        if (flashLight.enabled)
+        {
+            battery -= drainSpeed * Time.deltaTime;
+            if (battery <= 0)
+            {
+                battery = 0;
+                flashLight.enabled = false;
+            }
         }
     }
 }
